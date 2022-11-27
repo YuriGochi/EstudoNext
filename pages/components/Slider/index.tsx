9a28a5b838a1d5932e301ea58be6fefb43252000
Yuri Oliveira Gochi <yurioliveiragochi@gmail.com>
2022-11-27 20:49:26 -0300

    ajustes

--- pages/components/Slider/index.tsx
+++ pages/components/Slider/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './style.module.scss'
 import useEmblaCarousel from 'embla-carousel-react'
 import SliderDay from '../SliderDay';
+import classnames from 'classnames';
 
+// interface Meses {
+//     mes: string;
+//     key: string;
+// }
 const meses = [
     { mes: 'JAN', key: 'item-1'}, // remember to pass the key prop
     { mes: 'FEV', key: 'item-2'},
@@ -18,21 +23,130 @@ const meses = [
     { mes: 'DEZ', key: 'item-12'},
   ];
 
+// const Slider: React.FC = () => {
+//     const [emblaRef] = useEmblaCarousel()
+
+//     const array = meses
+
+//     const classes = classnames('slide_item', {
+//         'current': array.key === props.key,
+//         'future': date.month() > this.props.month,
+//         'past': date.month() < this.props.month
+//       });
+
+//     return(
+//         <>
+//         <div className={style.slider} ref={emblaRef}>
+//             <div className={style.slider_container}>
+//                 {meses.map((props) => {
+//                     return (
+//                         <button 
+//                         className={style.slider_item}>{props.mes}</button>
+//                     );
+//                 })}
+//             </div>
+//         </div>
+//         <SliderDay />
+//         <div className={style.att}>
+//             <div className={style.date}>
+//                 <div>
+//                     <p>Publicado em 14/11/2022 23H00</p>
+//                 </div>
+//                 <div>
+//                     <p>Atualizado em 15/11/2022 18H44</p>
+//                 </div>
+//             </div>
+//             <div className={style.share}>
+//                 <p>Compartilhe:</p>
+//                 <i>teste</i>
+//                 <i>teste</i>
+//                 <i>teste</i>
+//             </div>
+//         </div>
+//         </>
+//     )
+// };
+  
+//   export default Slider;
+
+  
+// const Slider: React.FC = () => {
+//     const [emblaRef] = useEmblaCarousel()
+//     const [isActive, setIsActive] = useState(false);
+
+//     const handleClick = () => {
+//       // 👇️ toggle
+//        setIsActive(current => !current);
+  
+//       // 👇️ or set to true
+//     //   setIsActive(true);
+//     };
+//     return(
+//         <>
+//         <div className={style.slider} ref={emblaRef}>
+//             <div className={style.slider_container}>
+//                 {meses.map((meses) => {
+//                     return (
+//                         <button onClick={handleClick}  
+//                         className={isActive ? style.slider_item_active : style.slider_item}>{meses.mes}</button>
+//                     );
+//                 })}
+//             </div>
+//         </div>
+//         <SliderDay />
+//         <div className={style.att}>
+//             <div className={style.date}>
+//                 <div>
+//                     <p>Publicado em 14/11/2022 23H00</p>
+//                 </div>
+//                 <div>
+//                     <p>Atualizado em 15/11/2022 18H44</p>
+//                 </div>
+//             </div>
+//             <div className={style.share}>
+//                 <p>Compartilhe:</p>
+//                 <i>teste</i>
+//                 <i>teste</i>
+//                 <i>teste</i>
+//             </div>
+//         </div>
+//         </>
+//     )
+// };
+  
+//   export default Slider;
 const Slider: React.FC = () => {
     const [emblaRef] = useEmblaCarousel()
-
     return(
         <>
         <div className={style.slider} ref={emblaRef}>
             <div className={style.slider_container}>
                 {meses.map((meses) => {
                     return (
-                        <div className={style.slider_item}>{meses.mes}</div>
+                        <button  
+                        className={style.slider_item}>{meses.mes}
+                        </button>
                     );
                 })}
             </div>
         </div>
         <SliderDay />
+        <div className={style.att}>
+            <div className={style.date}>
+                <div>
+                    <p>Publicado em 14/11/2022 23H00</p>
+                </div>
+                <div>
+                    <p>Atualizado em 15/11/2022 18H44</p>
+                </div>
+            </div>
+            <div className={style.share}>
+                <p>Compartilhe:</p>
+                <i>teste</i>
+                <i>teste</i>
+                <i>teste</i>
+            </div>
+        </div>
         </>
     )
 };
--- pages/components/SliderDay/index.tsx
+++ pages/components/SliderDay/index.tsx
@@ -3,37 +3,37 @@ import style from './style.module.scss'
 import useEmblaCarousel from 'embla-carousel-react'
 
 const dias = [
-    { dia: '01', key: 'item-1', semana: 'segunda'}, // remember to pass the key prop
-    { dia: '02', key: 'item-2'},
-    { dia: '03', key: 'item-3'},
-    { dia: '04', key: 'item-4'},
-    { dia: '05', key: 'item-5'},
-    { dia: '06', key: 'item-6'},
-    { dia: '07', key: 'item-7'},
-    { dia: '08', key: 'item-8'},
-    { dia: '09', key: 'item-9'},
-    { dia: '10', key: 'item-10'},
-    { dia: '11', key: 'item-11'},
-    { dia: '12', key: 'item-12'},
-    { dia: '13', key: 'item-13'},
-    { dia: '14', key: 'item-14'},
-    { dia: '15', key: 'item-15'},
-    { dia: '16', key: 'item-16'},
-    { dia: '17', key: 'item-17'},
-    { dia: '18', key: 'item-18'},
-    { dia: '19', key: 'item-19'},
-    { dia: '20', key: 'item-20'},
-    { dia: '21', key: 'item-21'},
-    { dia: '22', key: 'item-22'},
-    { dia: '23', key: 'item-23'},
-    { dia: '24', key: 'item-24'},
-    { dia: '25', key: 'item-25'},
-    { dia: '26', key: 'item-26'},
-    { dia: '27', key: 'item-27'},
-    { dia: '28', key: 'item-28'},
-    { dia: '29', key: 'item-29'},
-    { dia: '30', key: 'item-30'},
-    { dia: '31', key: 'item-31'},
+    { dia: '01', key: 'item-1', semana: 'SEG'}, // remember to pass the key prop
+    { dia: '02', key: 'item-2', semana: 'TER'},
+    { dia: '03', key: 'item-3', semana: 'QUA'},
+    { dia: '04', key: 'item-4', semana: 'QUI'},
+    { dia: '05', key: 'item-5', semana: 'SEX'},
+    { dia: '06', key: 'item-6', semana: 'SEG'},
+    { dia: '07', key: 'item-7', semana: 'TER'},
+    { dia: '08', key: 'item-8', semana: 'QUA'},
+    { dia: '09', key: 'item-9', semana: 'QUI'},
+    { dia: '10', key: 'item-10', semana: 'SEX'},
+    { dia: '11', key: 'item-11', semana: 'SEG'},
+    { dia: '12', key: 'item-12', semana: 'TER'},
+    { dia: '13', key: 'item-13', semana: 'QUA'},
+    { dia: '14', key: 'item-14', semana: 'QUI'},
+    { dia: '15', key: 'item-15', semana: 'SEG'},
+    { dia: '16', key: 'item-16', semana: 'TER'},
+    { dia: '17', key: 'item-17', semana: 'QUA'},
+    { dia: '18', key: 'item-18', semana: 'QUI'},
+    { dia: '19', key: 'item-19', semana: 'SEX'},
+    { dia: '20', key: 'item-20', semana: 'SEG'},
+    { dia: '21', key: 'item-21', semana: 'TER'},
+    { dia: '22', key: 'item-22', semana: 'QUA'},
+    { dia: '23', key: 'item-23', semana: 'QUI'},
+    { dia: '24', key: 'item-24', semana: 'SEG'},
+    { dia: '25', key: 'item-25', semana: 'TER'},
+    { dia: '26', key: 'item-26', semana: 'QUA'},
+    { dia: '27', key: 'item-27', semana: 'QUI'},
+    { dia: '28', key: 'item-28', semana: 'SEG'},
+    { dia: '29', key: 'item-29', semana: 'TER'},
+    { dia: '30', key: 'item-30', semana: 'QUA'},
+    { dia: '31', key: 'item-31', semana: 'QUI'},
   ];
 
 const SliderDay: React.FC = () => {
@@ -45,10 +45,12 @@ const SliderDay: React.FC = () => {
           {dias.map((dias) => {
               return (
                 <>
-                  <div className={style.slider_item}>{dias.dia}
+                  <div className={style.slider_item}>
+                  <button onClick={() => { 
+                            console.log(dias.key);
+                          }}  className={style.slider_dia}>{dias.dia}</button>
                     <div className={style.slider_semana}>{dias.semana}</div>
                   </div>
-                  
                 </>
               )
           })}
